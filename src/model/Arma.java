142a37d385a1a6a3bb8a1cd1d12c96100c7e29b8
Leonardo Piel <leonardo.piel@outlook.com>
2019-11-22 21:41:16 -0300

    Primeiro commit

---
+++ src/model/Arma.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author leona
+ */
+public class Arma {
+
+    private String nome;
+    private double coeficienteDano;
+    private int tipo;
+    /*
+     1 - a distância (30)
+     2 - perto (40)
+     */
+    public Arma(String nome, int tipo) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.setCoeficienteDano();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    
+     public double getCoeficienteDano() {
+        return coeficienteDano;
+    }
+
+    public void setCoeficienteDano() {
+        if(this.tipo == 1){
+            this.coeficienteDano = 30;
+        }else{
+            this.coeficienteDano = 40;
+        }
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+    
+    public void mostraArma(){
+        System.out.println("coef dano: "+this.coeficienteDano+ "\n"
+                + "nome: "+this.nome+"\n "
+                + "tipo: "+this.tipo);
+    }
+}
